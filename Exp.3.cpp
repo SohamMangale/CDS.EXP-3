77321934e9c78f7fbe5538a0d86522772d81ac14
SohamMangale <sohammangale60@gmail.com>
2024-08-15 12:16:56 +0530

    Create Exp.3.cpp

---
+++ Exp.3.cpp
@@ -0,0 +1,23 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 3
+#include<iostream>
+using namespace std;
+int main()
+{
+    //arithmetic operators
+    int a,b;
+    int sum,subtraction,multiplication,division;
+    cout<<"Enter the number-";
+    cin>>a>>b;
+    sum=a+b;
+    subtraction=a-b;
+    multiplication=a*b;
+    division=a/b;
+    cout<<"sum="<<sum<< endl;
+    cout<<"subtraction="<<subtraction<< endl;
+    cout<<"multiplication="<<multiplication<< endl;
+    cout<<"division="<<division<< endl;
+    return 0;
+}
