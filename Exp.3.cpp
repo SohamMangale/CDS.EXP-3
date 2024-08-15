317c0ba144b45567944e1a7551fbae8c5bb8cd98
SohamMangale <sohammangale60@gmail.com>
2024-08-15 12:21:50 +0530

    Create Exp.3.cpp

---
+++ Exp.3.cpp
@@ -0,0 +1,22 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 3
+#include<iostream>
+using namespace std;
+int main()
+{
+    //assignment operators
+    int a,b,c,d;
+    cout<<"Enter the value of a,b,c,d";
+    cin>>a>>b>>c>>d;
+    a-=3;
+    b*=3;
+    c/=3;
+    d&=3;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c=<<"<<c<<endl;
+    cout<<"d="<<d<<endl;
+    return 0;
+}
