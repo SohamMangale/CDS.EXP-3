901606948b7ccd712878036ea5b2f90466434db1
SohamMangale <sohammangale60@gmail.com>
2024-08-15 12:23:41 +0530

    Create Exp.3B.cpp

---
+++ Exp.3B.cpp
@@ -0,0 +1,30 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 3B
+#include<iostream>
+using namespace std;
+int main()
+{
+    //comparison operators
+    int a,b;
+    cout<<"Enter the values of a,b";
+    cin>>a>>b;
+    if(a==b)
+    {
+        cout<<"a==b"<<endl;
+    }
+   else if(a!=b)
+    {
+        cout<<"a!=b"<<endl;
+    }
+    if(a>b)
+    {
+        cout<<"a>b"<<endl;
+    }
+   else if(a<b)
+    {
+        cout<<"a<b"<<endl;
+    }
+    return 0;
+}
