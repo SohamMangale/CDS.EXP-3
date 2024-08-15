9916e1db6513ce0520092dc7f985163b9656805b
SohamMangale <sohammangale60@gmail.com>
2024-08-15 12:25:54 +0530

    Create Exp.3C.cpp

---
+++ Exp.3C.cpp
@@ -0,0 +1,19 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 3C
+#include<iostream>
+using namespace std;
+int main()
+{
+    //logical operators
+     int a,b,c,d,e;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    cout<<"\n" << (a > 1 && b < 10)<<endl;
+    cout<<"\n" << (a > 1 || b < 10)<<endl;
+    cout <<"\n"<<(!(a >1 && b <10))<<endl;
+    return 0;
+}
